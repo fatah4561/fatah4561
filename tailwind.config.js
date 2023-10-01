5d03b27ae6e6223e43ea7cb38bacc291af55c6f2
inventory_fatah <fatahathariq6@gmail.com>
2023-10-01 18:29:39 +0700

    splash page, delete dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        fontFamily: {
+          display: ['IBM Plex Mono', 'Menlo', 'monospace'],
+          body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+          monospace: ['Monospace2'],
+        },
+        colors: {
+          primary: {
+            50: '#f9fafb',
+            100: '#f3f4f6',
+            200: '#e5e7eb',
+            300: '#d1d5db',
+            400: '#9ca3af',
+            500: '#6b7280',
+            600: '#4b5563',
+            700: '#374151',
+            800: '#1f2937',
+            900: '#111827',
+          },
+          secondary: {
+            50: '#f0fdfa',
+            100: '#ccfbf1',
+            200: '#99f6e4',
+            300: '#5eead4',
+            400: '#2dd4bf',
+            500: '#14b8a6',
+            600: '#0d9488',
+            700: '#0f766e',
+            800: '#115e59',
+            900: '#134e4a',
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
